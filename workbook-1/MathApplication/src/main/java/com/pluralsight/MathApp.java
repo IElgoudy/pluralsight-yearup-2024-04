eede06d95402a352360634351123c8ad0727078c
ismail elgoudi <elgoudi93@gmail.com>
2024-04-11 14:38:35 -0700

    Completed MOD 3 exercise 2-MathApp

--- workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -1,9 +1,7 @@
 package com.pluralsight;
 
-public class MathApp
-{
-    public static void main(String[] args)
-    {
+public class MathApp {
+    public static void main(String[] args) {
         // find all 7 questions in your workbook on page 
         question1();
         question2();
@@ -16,10 +14,7 @@ public class MathApp
     }
 
     // write ONLY code for QUESTION 1 in this function
-    public static void question1()
-
-
-    {
+    public static void question1() {
         // Question 1:
         // declare variables here
         double bobSalary = 50000.0;
@@ -27,7 +22,7 @@ public class MathApp
 
 
         // code the logic to the problem here
-        double highestSalary = Math.max(bobSalary,garySalary);
+        double highestSalary = Math.max(bobSalary, garySalary);
 
         // use System.out.println() to display results
         System.out.println("Highest Salary " + highestSalary);
@@ -37,15 +32,14 @@ public class MathApp
 
 
     // write ONLY code for QUESTION 2 in this function
-    public static void question2()
-    {
+    public static void question2() {
         // Question 2:
         // declare variables here
         double carPrice = 5000;
         double truckPrice = 7000;
 
         // code the logic to the problem here
-        double cheapestVehicle = Math.min(carPrice,truckPrice);
+        double cheapestVehicle = Math.min(carPrice, truckPrice);
 
         // use System.out.println() to display results
         System.out.println("cheapest Vehicle " + cheapestVehicle);
@@ -54,8 +48,7 @@ public class MathApp
 
 
     // write ONLY code for QUESTION 3 in this function
-    public static void question3()
-    {
+    public static void question3() {
         // Question 3:
         // declare variables here
         double radius = 7.25;
@@ -70,8 +63,7 @@ public class MathApp
 
 
     // write ONLY code for QUESTION 4 in this function
-    public static void question4()
-    {
+    public static void question4() {
         // Question 4:
         // declare variables here
         double x = 5.0;
@@ -80,14 +72,13 @@ public class MathApp
         double squareRoot = Math.sqrt(x);
 
         // use System.out.println() to display results
-        System.out.println("The square Root of " +x+ "is" + squareRoot);
+        System.out.println("The square Root of " + x + "is" + squareRoot);
         // ex: System.out.println("The answer is " + answer);
     }
 
 
     // write ONLY code for QUESTION 5 in this function
-    public static void question5()
-    {
+    public static void question5() {
         // Question 5:
         // declare variables here
         double x1 = 5;
@@ -96,9 +87,9 @@ public class MathApp
         double y2 = 50;
 
         // code the logic to the problem here
-        double width = x2-x1;
-        double height = y2-y1;
-        double c = Math.pow(width,2)+ Math.pow(height,2);
+        double width = x2 - x1;
+        double height = y2 - y1;
+        double c = Math.pow(width, 2) + Math.pow(height, 2);
         double distance = Math.sqrt(c);
 
         // use System.out.println() to display results
@@ -110,8 +101,7 @@ public class MathApp
 
 
     // write ONLY code for QUESTION 6 in this function
-    public static void question6()
-    {
+    public static void question6() {
         // Question 6:
         // declare variables here
         double x = -3.8;
@@ -126,8 +116,7 @@ public class MathApp
 
 
     // write ONLY code for QUESTION 7 in this function
-    public static void question7()
-    {
+    public static void question7() {
         // Question 7:
         // declare variables here
         double randomNumber;
